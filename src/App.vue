6755c76d6f0de8c8fdcdb63a236eba1141688824
liuguanrui <amberyin@sina.cn>
2017-07-21 16:33:53 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<v-header>i am a header</v-header>
+		<div class="tab">
+			<router-link tag="li" :to="{name: 'word'}">
+				<a href="">商品</a>
+			</router-link>
+			<router-link tag="li" to="/hello">
+				<a href="">评论</a>
+			</router-link>
+			<router-link tag="li" to="/word">
+				<a href="">商家</a>
+			</router-link>
+		</div>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+export default {
+  name: 'app',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus">
+.tab
+  display: flex
+  height: 40px
+  line-height: 40px
+  & li
+    list-style: none
+    flex:1
+
+</style>
