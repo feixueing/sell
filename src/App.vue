0ae42bc930e2ae762ff47ed09324d743640b7ddb
liuguanrui <amberyin@sina.cn>
2017-07-21 20:43:34 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
 	<div id="app">
 		<v-header>i am a header</v-header>
-		<div class="tab">
-			<router-link tag="li" :to="{name: 'word'}">
-				<a href="">商品</a>
+		<div class="tab border-1px">
+			<router-link :to="{name: 'word'}">
+				商品
 			</router-link>
-			<router-link tag="li" to="/hello">
-				<a href="">评论</a>
+			<router-link to="/hello">
+				评论
 			</router-link>
-			<router-link tag="li" to="/word">
-				<a href="">商家</a>
+			<router-link to="/word">
+				商家
 			</router-link>
 		</div>
 		<router-view></router-view>
@@ -28,12 +28,20 @@ export default {
 </script>
 
 <style lang="stylus">
+@import "./common/stylus/mixin.styl"
 .tab
   display: flex
+  width: 100%
   height: 40px
   line-height: 40px
-  & li
+  border-1px(rgba(7,17,27,0.1))
+  & a
     list-style: none
     flex:1
-
+    text-align: center
+    font-size: 14px
+    color: rgb(77,85,93)
+    &.active
+      color: rgb(240,20,20)
+	
 </style>
